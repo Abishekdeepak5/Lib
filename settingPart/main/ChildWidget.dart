632b965a00a31a8b9317eceacee9dab7ed455b88
aathi-immortal <89212658+aathi-immortal@users.noreply.github.com>
2023-12-10 00:00:04 +0530

    Add files via upload

---
+++ settingPart/main/ChildWidget.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+// parent concrete class for all child button in the setting page
+
+class ChildWidget extends StatefulWidget {
+  final onPressed;
+  final placeHolder;
+  const ChildWidget(
+      {Key? key, required this.onPressed, required this.placeHolder})
+      : super(key: key);
+
+  @override
+  State<ChildWidget> createState() => _ChildWidgetState();
+}
+
+class _ChildWidgetState extends State<ChildWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: EdgeInsets.only(left: 40),
+      title: Padding(
+        padding: EdgeInsets.only(bottom: 8),
+        child: Container(
+          decoration: const BoxDecoration(
+            border: BorderDirectional(
+              bottom: BorderSide(
+                color: Colors.black12,
+                style: BorderStyle.solid,
+              ),
+            ),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                widget.placeHolder,
+                style: const TextStyle(
+                  fontSize: 17, // Adjust the font size as needed
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              IconButton(
+                onPressed: widget.onPressed,
+                icon: Icon(Icons.arrow_forward_ios_outlined),
+                iconSize: 18,
+                color: Color.fromARGB(255, 172, 170, 170),
+              ) // Adjust the icon size as needed
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

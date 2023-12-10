82f7d74ad1cc1c4a7a56866c537ea79f71daeba4
Abishekdeepak5 <143536892+Abishekdeepak5@users.noreply.github.com>
2023-12-10 11:12:02 +0530

    Update ProfileSection.dart

--- settingPart/ProfileSection.dart
+++ settingPart/ProfileSection.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:new_app/settingPart/main/ChildWidget.dart';
+import 'package:simple_layout/settingPart/main/ChildWidget.dart';
 
-import 'package:new_app/settingPart/main/HeadingWidget.dart';
+import 'package:simple_layout/settingPart/main/HeadingWidget.dart';
 
 // profile  section class
 
